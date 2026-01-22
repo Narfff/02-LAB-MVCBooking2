3cb6928e55570c6b9e9381468c1d6b44fe240518
Fran Habijanic <fran.habijanic@gmail.com>
2026-01-22 16:54:28 +0100

    1

---
+++ MVCBooking/Controllers/BookingController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCBooking.Models;
+
+namespace MVCBooking.Controllers
+{
+	public class BookingController : Controller
+	{
+		private static List<HotelBooking> bookings = new List<HotelBooking>();
+
+		public IActionResult Index()
+		{
+			return View(bookings);
+		}
+
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+
+		public IActionResult CreateBooking(HotelBooking booking)
+		{
+			booking.Id = bookings.Count + 1;
+			bookings.Add(booking);
+			return RedirectToAction("Index");
+		}
+	}
+}
